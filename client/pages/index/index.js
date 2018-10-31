2a07ad1b604dc94238132d3403b217b1e9777e14
weedustzhao(赵青松) <weedustzhao@tencent.com>
2018-10-31 21:17:06 +0800

    实现动画效果

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,9 +1,25 @@
 Page({
   data: {
-    
+    animationDatas: [],
   },
-  onLoad: function () {
-    
+  aniMain: function () {
+    const query = wx.createSelectorQuery();
+    const ref = query.selectAll('.ani-init');
+    ref.fields({}, function (res) {
+      const animationDatas = [];
+      res.forEach(function (node, index) {
+        const animation = wx.createAnimation({
+          duration: 200,
+          delay: 200 * index,
+        });
+        animation.opacity(1).top(0).step();
+        animationDatas.push(animation.export());
+      });
+      this.setData({ animationDatas });
+    }.bind(this)).exec();
+  },
+  onShow: function () {
+    this.aniMain();
   }
 })
 
