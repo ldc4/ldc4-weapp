e591245f2d4ec0e53b820ab82c3e6b212b49553a
weedust <544420314@qq.com>
2018-11-04 22:43:56 +0800

    修复数据库bullet表的字段

--- server/mysql/bullet.sql
+++ server/mysql/bullet.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 2018-11-04 06:14:32
--- 服务器版本： 5.7.18
--- PHP Version: 5.6.30
+-- Generation Time: 2018-11-04 14:39:13
+-- 服务器版本： 5.7.24
+-- PHP Version: 5.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,16 +26,12 @@ SET time_zone = "+00:00";
 -- 表的结构 `bullet`
 --
 
-CREATE TABLE `bullet` (
+CREATE TABLE IF NOT EXISTS `bullet` (
   `id` int(11) NOT NULL,
   `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '弹幕内容',
   `createdTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `userInfo` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户信息'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='首页的弹幕';
-
---
--- 转存表中的数据 `bullet`
---
+  `userInfo` text COLLATE utf8mb4_unicode_ci COMMENT '用户信息'
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='首页的弹幕';
 
 --
 -- Indexes for dumped tables
@@ -50,15 +44,14 @@ ALTER TABLE `bullet`
   ADD PRIMARY KEY (`id`);
 
 --
--- 在导出的表使用AUTO_INCREMENT
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- 使用表AUTO_INCREMENT `bullet`
+-- AUTO_INCREMENT for table `bullet`
 --
 ALTER TABLE `bullet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
